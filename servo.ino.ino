fcaaeb47005a05aced96ff70f5cf29721c76d865
Nagaraju <urlaguntanagaraju1@gmail.com>
2017-03-26 17:26:50 +0530

    Add files via upload

---
+++ servo.ino.ino
@@ -0,0 +1,22 @@
+#include <Servo.h>       //Servo library
+ 
+ Servo name_servo;      //initialize a servo object for the connected servo  
+
+ int servo_position =0;
+ void setup() 
+ { 
+  name_servo.attach(9);   // attach the signal pin of servo to pin9 of arduino
+ } 
+ 
+ void loop() 
+ { 
+  for (servo_position=0;servo_position<=180;servo_position +=1){
+    name_servo.write(servo_position);
+    delay(10);             
+  }
+  for(servo_position=180; servo_position>=0;servo_position-=1)
+  {
+    name_servo.write(servo_position); 
+    }
+delay(10);
+ }  
